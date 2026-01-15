9dc0c724c669b30956b5008341db3ea23c8385a4
ashmilakp-dotcom <ashmilakp@vjec.ac.in>
2026-01-16 00:21:55 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,3 @@
-CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    salary INTEGER
-);
-
-INSERT INTO employee VALUES (1, 'Alice', 50000);
-INSERT INTO employee VALUES (2, 'Bob', 60000);
-INSERT INTO employee VALUES (3, 'Carol', 70000);
+SELECT patient_name
+FROM PATIENT
+WHERE age > 50;
